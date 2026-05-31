2f9c8b5e53e51d6dcb378ebae86246c8a1ea5daf
RMplayTV <raulmatoslt01@gmail.com>
2026-05-31 15:59:28 -0300

    Update addon.py

--- plugin.video.rmtvgold/addon.py
+++ plugin.video.rmtvgold/addon.py
@@ -1,25 +1,41 @@
-plugin.video.rmtvgold/addon.py
-
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<addons>
-    <addon id="plugin.video.rmtvgold"
-           name="RM TV GOLD"
-           version="1.0.0"
-           provider-name="RMplayTV">
 
-        <requires>
-            <import addon="xbmc.python" version="3.0.0"/>
-        </requires>
+<addon id="plugin.video.rmtvgold"
+    name="RM TV GOLD"
+    version="1.0.0"
+    provider-name="RMplayTV">
 
-        <extension point="xbmc.python.pluginsource" library="addon.py">
-            <provides>video</provides>
-        </extension>
+```
+<requires>
+    <import addon="xbmc.python" version="3.0.0"/>
+</requires>
 
-        <extension point="xbmc.addon.metadata">
-            <summary>RM TV GOLD</summary>
-            <description>TV, Filmes, Séries, Animes e Desenhos</description>
-            <platform>all</platform>
-        </extension>
+<extension point="xbmc.python.pluginsource"
+           library="addon.py">
+    <provides>video</provides>
+</extension>
 
-    </addon>
-</addons>
+<extension point="xbmc.addon.metadata">
+    <summary lang="pt_BR">RM TV GOLD</summary>
+
+    <description lang="pt_BR">
+        TV AO VIVO, FILMES, SÉRIES, ANIMES,
+        DESENHOS, PESQUISA E CONFIGURAÇÕES.
+    </description>
+
+    <platform>all</platform>
+
+    <assets>
+        <icon>icon.png</icon>
+        <fanart>fanart.jpg</fanart>
+    </assets>
+
+    <license>Personal Use</license>
+
+    <news>
+        v1.0.0 - Versão inicial do RM TV GOLD.
+    </news>
+</extension>
+```
+
+</addon>
