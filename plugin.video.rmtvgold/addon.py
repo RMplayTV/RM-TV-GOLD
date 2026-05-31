f67d43ff3a4e146c53615fac779718ecebcca719
RMplayTV <raulmatoslt01@gmail.com>
2026-05-31 16:01:12 -0300

    Refactor addon.py to use Python syntax

--- plugin.video.rmtvgold/addon.py
+++ plugin.video.rmtvgold/addon.py
@@ -1,41 +1,94 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+# -*- coding: utf-8 -*-
 
-<addon id="plugin.video.rmtvgold"
-    name="RM TV GOLD"
-    version="1.0.0"
-    provider-name="RMplayTV">
+import sys
+import urllib.parse
+import xbmcplugin
+import xbmcgui
 
-```
-<requires>
-    <import addon="xbmc.python" version="3.0.0"/>
-</requires>
+HANDLE = int(sys.argv[1])
 
-<extension point="xbmc.python.pluginsource"
-           library="addon.py">
-    <provides>video</provides>
-</extension>
 
-<extension point="xbmc.addon.metadata">
-    <summary lang="pt_BR">RM TV GOLD</summary>
+# ---------------------------
+# FUNÇÃO PARA CRIAR URL
+# ---------------------------
+def build_url(query):
+    return sys.argv[0] + '?' + urllib.parse.urlencode(query)
 
-    <description lang="pt_BR">
-        TV AO VIVO, FILMES, SÉRIES, ANIMES,
-        DESENHOS, PESQUISA E CONFIGURAÇÕES.
-    </description>
 
-    <platform>all</platform>
+# ---------------------------
+# ADICIONAR MENU
+# ---------------------------
+def add_item(title, action, is_folder=True):
+    url = build_url({'action': action})
+    li = xbmcgui.ListItem(label=title)
+    xbmcplugin.addDirectoryItem(HANDLE, url, li, is_folder)
 
-    <assets>
-        <icon>icon.png</icon>
-        <fanart>fanart.jpg</fanart>
-    </assets>
 
-    <license>Personal Use</license>
+# ---------------------------
+# MENU PRINCIPAL
+# ---------------------------
+def home():
+    add_item("📺 TV AO VIVO", "tv")
+    add_item("🎬 FILMES", "filmes")
+    add_item("📺 SÉRIES", "series")
+    add_item("🎌 ANIMES", "animes")
+    add_item("🧸 DESENHOS", "desenhos")
+    add_item("🔞 +18", "adulto")
+    add_item("🔎 PESQUISA", "search")
+    add_item("⚙️ CONFIGURAÇÕES", "config")
 
-    <news>
-        v1.0.0 - Versão inicial do RM TV GOLD.
-    </news>
-</extension>
-```
+    xbmcplugin.endOfDirectory(HANDLE)
 
-</addon>
+
+# ---------------------------
+# SUBMENUS (PLACEHOLDER)
+# ---------------------------
+def placeholder(nome):
+    li = xbmcgui.ListItem(label=f"Entrou em: {nome}")
+    xbmcplugin.addDirectoryItem(HANDLE, "", li, False)
+    xbmcplugin.endOfDirectory(HANDLE)
+
+
+# ---------------------------
+# ROUTER (NAVEGAÇÃO)
+# ---------------------------
+def router(param):
+    if param is None:
+        home()
+
+    elif param == "tv":
+        placeholder("TV AO VIVO")
+
+    elif param == "filmes":
+        placeholder("FILMES")
+
+    elif param == "series":
+        placeholder("SÉRIES")
+
+    elif param == "animes":
+        placeholder("ANIMES")
+
+    elif param == "desenhos":
+        placeholder("DESENHOS")
+
+    elif param == "adulto":
+        placeholder("+18")
+
+    elif param == "search":
+        placeholder("PESQUISA")
+
+    elif param == "config":
+        placeholder("CONFIGURAÇÕES")
+
+    else:
+        home()
+
+
+# ---------------------------
+# CAPTURA DE PARAMETROS
+# ---------------------------
+params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
+
+action = params.get('action')
+
+router(action)
